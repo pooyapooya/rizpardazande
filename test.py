ab8ab671f30d048c066a6266dec3a8e98417afd1
Your Name <you@example.com>
2016-07-05 12:48:19 +0430

    done:D

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import sys
 from easygui import *
-# from getNBlinkN2nanpyLCD import taha
+from getNBlinkN2nanpyLCD import taha
 
 msg = "Hi, Please type a positive integer :)"
 n = integerbox(msg=msg, default=1, lowerbound=1)
-# taha(n)
+taha(n)
 
