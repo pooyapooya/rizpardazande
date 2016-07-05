4a8a63ea419f2614abd745307a952c4bce0ac8df
Pooya Mosaddegh <pooya.mosaddegh72@gmail.com>
2016-07-05 13:14:01 +0430

    ui really completed

--- test.py
+++ test.py
@@ -2,7 +2,13 @@ import sys
 from easygui import *
 from getNBlinkN2nanpyLCD import taha
 
+msgbox('salam')
 msg = "Hi, Please type a positive integer :)"
 n = integerbox(msg=msg, default=1, lowerbound=1)
 taha(n)
+msgbox(msg='Your number is {}. :)'.format(n))
 
+
+
+def pooya_print(msg):
+    msgbox(msg=msg)
